36aadc34268f13be7dd6d6f63fff144f4d0b59ae
xiaohanfu123 <shawnx1995@gmail.com>
2022-08-28 16:11:18 +0000

    try

---
+++ functions/map_builtin/mapTest.js
@@ -0,0 +1,7 @@
+const lighthouses = ["Gibraltar Point", "Peggy's Point", "Cove Island", "Discovery Island", "Cape Scott", "Point Clark", "Kincardine"];
+
+const numbers = lighthouses.map((x) => x.length)
+console.log(numbers)
+
+// write code here to use map
+// It should output: [ 15, 13, 11, 16, 10, 11, 10 ]
